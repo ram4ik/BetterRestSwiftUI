2dace98713bc2be370abba9d0f60dcd4de672e35
ram4ik <ram4ik@hotmail.com>
2023-10-20 05:12:28 -0700

    working with dates

--- BetterRestSwiftUI/ContentView.swift
+++ BetterRestSwiftUI/ContentView.swift
@@ -11,14 +11,22 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("Dates")
+            Text(Date.now, format: .dateTime.hour().minute())
+            Text(Date.now, format: .dateTime.day().month().year())
+            
+            Text(Date.now.formatted(date: .long, time: .shortened))
         }
     }
     
     func exampleDates() {
-        let now = Date.now
-        let tommorow = Date.now.addingTimeInterval(86400)
-        let range = now...tommorow
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
