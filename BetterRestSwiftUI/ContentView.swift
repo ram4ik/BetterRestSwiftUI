e1a2dfa5865bda773872eee0fd241ec75eb92613
ram4ik <ram4ik@hotmail.com>
2023-10-20 05:03:03 -0700

    dates range

--- BetterRestSwiftUI/ContentView.swift
+++ BetterRestSwiftUI/ContentView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
     
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            .labelsHidden()
-            .padding()
+        VStack {
+            Text("Dates")
+        }
     }
     
     func exampleDates() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+        let now = Date.now
+        let tommorow = Date.now.addingTimeInterval(86400)
+        let range = now...tommorow
     }
 }
 
