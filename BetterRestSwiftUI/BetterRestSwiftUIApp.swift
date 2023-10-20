977837c53466764e57af491a495cb97ae8091f2a
ram4ik <ram4ik@hotmail.com>
2023-10-20 04:39:31 -0700

    Initial Commit

---
+++ BetterRestSwiftUI/BetterRestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestSwiftUIApp.swift
+//  BetterRestSwiftUI
+//
+//  Created by admin on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
